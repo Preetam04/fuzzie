7fd98e3b5c3c137c4081e209875aec715c63ef58
Preetam <preetampatil2324@gmail.com>
2025-01-01 19:23:04 +0530

    Added Sidebar & Infobar

---
+++ app/(main)/(pages)/billing/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Billing({}: Props) {
+  return <div>Billing</div>;
+}
---
+++ app/(main)/(pages)/workflows/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function WorkFlows({}: Props) {
+  return <div>WorkFlows</div>;
+}
