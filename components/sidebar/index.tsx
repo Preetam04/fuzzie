122afc33aa058328861b6ac0cb44c4b903d83e3f
Preetam <preetampatil2324@gmail.com>
2025-01-03 20:53:28 +0530

    added prisma file

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -18,7 +18,7 @@ const MenuOptions = () => {
   const pathName = usePathname();
 
   return (
-    <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
+    <nav className=" dark:bg-black h-screen overflow-scroll overflow-x-hidden  justify-between flex items-center flex-col  gap-10 py-6 px-2">
       <div className="flex items-center justify-center flex-col gap-8">
         <Link className="flex font-bold flex-row " href="/">
           fuzzie.
